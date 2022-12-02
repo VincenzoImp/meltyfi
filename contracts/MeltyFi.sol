6b6b40be56f5f3407e94fbefd46fc104b6e3e04f
VincenzoImp <61617279+VincenzoImp@users.noreply.github.com>
2022-12-02 19:39:49 +0100

    update

---
+++ contracts/MeltyFi.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MeltyFi {
+
+    struct Lottery { 
+        uint sold_tickets;
+        uint total_ticket_supply;
+        //function price_distribution_function;
+        uint deadline;
+        //IERC721 NFT list;
+    }
+    //lottery_list;
+}
---
+++ contracts/WonkaTicket.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../interfaces/IERC1155.sol";
+
+contract WonkaTicket is IERC1155 {
+    
+}
