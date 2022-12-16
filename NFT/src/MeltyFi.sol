e2c29ac7ccfd6cd384864d5abb7e82e3e2c059ed
VincenzoImp <61617279+VincenzoImp@users.noreply.github.com>
2022-12-16 22:15:34 +0100

    update

--- NFT/src/MeltyFi.sol
+++ NFT/src/MeltyFi.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
+// VincenzoImp MeltyFi (last updated v0.0.1) (NFT/src/MeltyFi.sol)
+
 pragma solidity ^0.8.0;
 
-error MeltyFi__NotOwner();
+import "./ChocoChip.sol";
+import "./WonkaTicket.sol";
 
 contract MeltyFi {
 
     address private owner;
+    
+    ChocoChip public immutable choco;
 
     struct Lottery { 
         uint sold_tickets;
@@ -14,9 +19,15 @@ contract MeltyFi {
         uint deadline;
         //IERC721 NFT list;
     }
-    //lottery_list;
-    modifier onlyOwner {
-        if (msg.sender != owner) revert MeltyFi__NotOwner();
-        _;
+
+    constructor() {
+        choco = new ChocoChip("ChocoChip", "CHCH", 10000000, address(this));
+        
+    }
+    function transferMelty() public {
+        choco.approve(msg.sender, 10);
+    }
+    function saldo() public view returns (uint256){
+        return choco.balanceOf(msg.sender);
     }
 }
