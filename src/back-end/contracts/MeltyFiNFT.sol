346260a2b302a338f99623b9298515d3bcbc571b
VincenzoImp <61617279+VincenzoImp@users.noreply.github.com>
2023-01-02 20:45:10 +0100

    comment added to src/back-end/contracts/MeltyFiNFT.sol

--- src/back-end/contracts/MeltyFiNFT.sol
+++ src/back-end/contracts/MeltyFiNFT.sol
@@ -1,73 +1,130 @@
 // SPDX-License-Identifier: MIT
+
+/**
+  * MeltyFiNFT is the contract that run the protocol of the MeltyFi platform.
+  * It manages the creation, cancellation and conclusion of lotteries, as well as the
+  * sale and refund of WonkaBars for each lottery, and also reward good users with ChocoChips.
+  * The contract allows users to create a lottery by choosing their NFT to put as lottery prize,
+  * setting an expiration date and defining a price in Ether for each WonkaBar sold.
+  * When a lottery is created, the contract will be able to mint a fixed amount of WonkaBars 
+  * (setted by lottery owner) for the lottery. These WonkaBars are sold to users interested 
+  * in participating in the lottery and money raised are sent to the lottery owner (less some fees). 
+  * Once the expiration date is reached, the contract selects a random WonkaBar
+  * holder as the winner, who receives the prize NFT. Plus every wonkabar holder is rewarded 
+  * with ChocoCips. If the lottery is cancelled by the owner beafore the expiration date, 
+  * the contract refunds WonkaBars holders with Ether of the lottery owners. Plus every 
+  * wonkabar holder is rewarded with ChocoCips.
+  * @author MeltyFi Team
+  * @version 0.1.0
+  */
+
 pragma solidity ^0.8.9;
 
-import "./ChocoChip.sol";
-import "./MeltyFiDAO.sol";
-import "./WonkaBar.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "./ChocoChip.sol"; /// ChocoChip contract is the governance token of the MeltyFi platform.
+import "./MeltyFiDAO.sol"; /// MeltyFiDAO contract is the governance contract of the MeltyFi platform.
+import "./WonkaBar.sol"; /// WonkaBar contract is the utility token of the MeltyFi platform.
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol"; /// IERC721 interface defines the required methods for an ERC721 contract.
+import "@openzeppelin/contracts/utils/Address.sol"; /// Address library provides utilities for working with addresses.
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol"; /// EnumerableSet library provides a data structure for storing and iterating over sets of values.
 
 contract MeltyFiNFT is Ownable {
-
+    
+    /// Enum for the possible states of a lottery.
     enum lotteryState { ACTIVE, CANCELLED, CONCLUDED, INVALID }
 
+    /// Using Address for address type.
     using Address for address;
+    /// Using EnumerableSet for EnumerableSet.AddressSet type.
     using EnumerableSet for EnumerableSet.AddressSet;
+    /// Using EnumerableSet for EnumerableSet.UintSet type.
     using EnumerableSet for EnumerableSet.UintSet;
 
+    /// Struct for storing the information of a lottery.
     struct Lottery {
+        /// Expiration date of the lottery.
         uint256 expirationDate;
+        /// ID of the lottery.
         uint256 id;
+        /// Owner of the lottery.
         address owner;
+        /// Prize NFT contract of the lottery.
         IERC721 prizeContract;
+        /// Prize NFT token ID of the lottery.
         uint256 prizeTokenId;
+        /// State of the lottery.
         lotteryState state;
+        /// Winner of the lottery.
         address winner;
-        uint256 wonkaBarsSold;  /// equal to totalSupply by costruction
-        uint256 wonkaBarsMelted;
+        /// Number of WonkaBars sold for the lottery.
+        uint256 wonkaBarsSold;
+        /// Maximum supply of WonkaBars for the lottery.
         uint256 wonkaBarsMaxSupply;
+        /// Price of each WonkaBar for the lottery, in ChocoChips
         uint256 wonkaBarPrice;
     }
 
+    /// Instance of the ChocoChip contract.
     ChocoChip internal immutable _contractChocoChip;
+    /// Instance of the WonkaBar contract.
     WonkaBar internal immutable _contractWonkaBar;
+    /// Instance of the MeltyFiDAO contract.
     MeltyFiDAO internal immutable _contractMeltyFiDAO;
 
+    /// Amount of ChocoChips per Ether.
     uint256 internal immutable _amountChocoChipPerEther;
+    /// Percentage of royalties to be paid to the MeltyFiDAO.
     uint256 internal immutable _royaltyDAOPercentage;
+    /// Upper limit percentage of the value of the prize NFT.
     uint256 internal immutable _upperLimitPercentage;
 
+    /// Total number of lotteries created.
     uint256 internal _totalLotteriesCreated;
 
+    /// Mapping from lottery ID to lottery information.
     mapping(uint256 => Lottery) internal _lotteryIdToLottery;
 
+    /// Mapping from lottery owner to set of valid lottery IDs.
     mapping(address => EnumerableSet.UintSet) internal _lotteryOwnerToValidLotteryIds;
+    /// Mapping from prize NFT contract to set of valid lottery IDs.
     mapping(address => EnumerableSet.UintSet) internal _prizeContractToValidLotteryIds;
+    /// Mapping from WonkaBar holder to set of valid lottery IDs.
     mapping(address => EnumerableSet.UintSet) internal _wonkaBarHolderToValidLotteryIds;
 
+    /// Set of valid lottery IDs.
     EnumerableSet.UintSet internal _validLotteries;
 
+    /// Set of valid lottery owners.
     EnumerableSet.AddressSet internal _validLotteryOwners;
+    /// Set of valid prize NFT contracts.
     EnumerableSet.AddressSet internal _validPrizeContracts;
+    /// Set of valid WonkaBar holders.
     EnumerableSet.AddressSet internal _validWonkaBarHolders;
     
+    /**
+      * Constructor of the MeltyFiNFT contract.
+      *
+      * @param contractChocoChip instance of the ChocoChip contract.
+      * @param contractWonkaBar instance of the WonkaBar contract.
+      * @param contractMeltyFiDAO instance of the MeltyFiDAO contract.
+      */
     constructor(
         ChocoChip contractChocoChip,
         WonkaBar contractWonkaBar,
         MeltyFiDAO contractMeltyFiDAO
     )
     {
+        /// The ChocoChip contract and the MeltyFiDAO token must be the same contract.
         require(
             address(contractChocoChip) == address(contractMeltyFiDAO.token()),
             ""
         );
-
+        /// The caller must be the owner of the ChocoChip contract.
         require(
             contractChocoChip.owner() == _msgSender(), 
             ""
         );
 
+        /// Initializing the instance variables.
         _contractChocoChip = contractChocoChip;
         _contractWonkaBar = contractWonkaBar;
         _contractMeltyFiDAO = contractMeltyFiDAO;
@@ -78,40 +135,69 @@ contract MeltyFiNFT is Ownable {
         _totalLotteriesCreated = 0;
     }
 
+    /**
+      * Returns the address of the ChocoChip contract.
+      *
+      * @return address of the ChocoChip contract.
+      */
     function _addressChocoChip() internal view virtual returns (address) {
         return address(_contractChocoChip);
     }
 
+    /**
+      * Returns the address of the WonkaBar contract.
+      *
+      * @return address of the WonkaBar contract.
+      */
     function _addressWonkaBar() internal view virtual returns (address) {
         return address(_contractWonkaBar);
     }
 
+    /**
+      * Returns the address of the MeltyFiDAO contract.
+      *
+      * @return address of the MeltyFiDAO contract.
+      */
     function _addressMeltyFiDAO() internal view virtual returns (address) {
         return address(_contractMeltyFiDAO);
     }
 
+    /**
+      * Calculates the amount to be repaid to a user when a lottery is cancelled or has no WonkaBars sold.
+      * The amount is equal to the number of WonkaBars the user bought multiplied by the price of each WonkaBar.
+      *
+      * @param lottery information of the lottery.
+      * @return amount to be repaid.
+      */
     function _amountToRepay(Lottery memory lottery) internal view virtual returns (uint256) {
         return lottery.wonkaBarsSold * lottery.wonkaBarPrice;
     }
 
+    /**
+      * Calculates the amount to be refunded to a user when a lottery is cancelled or has no WonkaBars sold.
+      * The amount is equal to the number of ChocoChips the user paid for the WonkaBars minus the royalties to be paid to the MeltyFiDAO.
+      *
+      * @param lottery information of the lottery.
+      * @param addressToRefund address of the user to be refunded.
+      * @return amount to be refunded.
+      */
     function _amountToRefund(Lottery memory lottery, address addressToRefund) internal view virtual returns (uint256) {
         return _contractWonkaBar.balanceOf(addressToRefund, lottery.id) * lottery.wonkaBarPrice;
     }
 
     function _isValidLottery(Lottery memory lottery) internal view virtual returns (bool) {
+        uint256 supplyWonkaBars = _contractWonkaBar.totalSupply(lottery.id);
         bool result = !(
-                lottery.state == lotteryState.INVALID
+            lottery.state == lotteryState.INVALID
             ||
             (
-                lottery.state == lotteryState.CANCELLED 
+                (
+                    lottery.state == lotteryState.CANCELLED 
+                    ||
+                    lottery.state == lotteryState.CONCLUDED
+                )
                 && 
-                lottery.wonkaBarsSold == 0
-            ) 
-            || 
-            (
-                lottery.state == lotteryState.CONCLUDED
-                &&
-                lottery.wonkaBarsSold == lottery.wonkaBarsMelted
+                supplyWonkaBars == 0
             )
         );
         return result;
@@ -178,23 +264,38 @@ contract MeltyFiNFT is Ownable {
         Lottery memory lottery = _lotteryIdToLottery[lotteryId];
         return _isValidLottery(lottery);
     }
-
+    
+    /**
+      * Creates a new lottery.
+      *
+      * @param expirationDate expiration date of the lottery.
+      * @param prizeContract address of the prize NFT contract.
+      * @param prizeTokenId ID of the prize NFT token.
+      * @param wonkaBarPrice price of each WonkaBar, in ChocoChips.
+      * @param wonkaBarsMaxSupply maximum supply of WonkaBars for the lottery.
+      * @return lotteryId of the newly created lottery.
+      */
     function createLottery(
+        uint256 expirationDate,
         IERC721 prizeContract,
         uint256 prizeTokenId,
-        uint256 wonkaBarsMaxSupply,
-        uint256 wonkabarPrice,
-        uint256 expirationDate
-    ) public virtual 
+        uint256 wonkaBarPrice,
+        uint256 wonkaBarsMaxSupply
+    ) 
+        public virtual 
+        returns (uint256)
     {
+        /// 
         require(
             (wonkaBarsMaxSupply / 100) * _upperLimitPercentage >= 1,
             ""
         );
+        /// The expiration date must be in the future.
         require(
             block.timestamp < expirationDate,
             ""
         );
+        ///
         require(
             prizeContract.ownerOf(prizeTokenId) == _msgSender(),
             ""
@@ -203,8 +304,10 @@ contract MeltyFiNFT is Ownable {
         prizeContract.safeTransferFrom(_msgSender(), address(this), prizeTokenId);
 
         uint256 lotteryId = _totalLotteriesCreated;
+        /// Incrementing the total number of lotteries created.
         _totalLotteriesCreated += 1;
 
+        /// Creating the lottery.
         Lottery memory lottery = Lottery(
             expirationDate,
             lotteryId,
@@ -214,9 +317,8 @@ contract MeltyFiNFT is Ownable {
             lotteryState.ACTIVE,
             address(0),
             0,
-            0,
             wonkaBarsMaxSupply,
-            wonkabarPrice
+            wonkaBarPrice
         );
 
         _lotteryIdToLottery[lotteryId] = lottery;
@@ -229,8 +331,15 @@ contract MeltyFiNFT is Ownable {
         _validLotteryOwners.add(_msgSender());
         _validPrizeContracts.add(address(prizeContract));
 
+        return lotteryId;
     }
 
+    /**
+      * Buys WonkaBars for a lottery.
+      *
+      * @param lotteryId ID of the lottery.
+      * @param amount amount of Ether paid for the WonkaBars.
+      */
     function buyWonkaBars(
         uint256 lotteryId, 
         uint256 amount
@@ -241,18 +350,24 @@ contract MeltyFiNFT is Ownable {
 
         uint256 totalSpending = amount * lottery.wonkaBarPrice;
 
+        /// The lottery must be active.
+        require(
+            lottery.state == lotteryState.ACTIVE,
+            ""
+        );
+        /// The total supply of WonkaBars must not exceed the maximum supply allowed.
         require(
             lottery.wonkaBarsSold + amount <= lottery.wonkaBarsMaxSupply,
             ""
         );
-
+        ///
         require(
             (_contractWonkaBar.balanceOf(_msgSender(), lotteryId) + amount) / lottery.wonkaBarsMaxSupply 
             <= 
             _upperLimitPercentage / 100,
             ""
         );
-
+        ///
         require(
             msg.value >= totalSpending,
             ""
