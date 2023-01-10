c5422f84662dd28751feb593ae84ccd47bb9a5ba
Benigno Ansanelli <ansanellibenigno@gmail.com>
2023-01-10 23:27:45 +0100

    completed view of lotteries

--- src/front-end/meltyfi.nft/src/pages/Lotteries.jsx
+++ src/front-end/meltyfi.nft/src/pages/Lotteries.jsx
@@ -4,12 +4,61 @@ import {ThirdwebSDK} from "@thirdweb-dev/sdk";
 import {useAddress} from "@thirdweb-dev/react";
 import MeltyFiNFT from "../contracts/MeltyFiNFT.json";
 import {useEffect, useState} from "react";
+import {addressMeltyFiNFT} from "../App";
 
 
-function renderLotteries(lotteries) {
-    let text = <p>testo html<br/>accapo</p>
+
+async function getNFTData(sdk, meltyfi, lottery) {
+    const contract = await sdk.getContract(await meltyfi.call("getLotteryPrizeContract", lottery), "nft-collection");
+    const token = await meltyfi.call("getLotteryPrizeTokenId", lottery);
+    const metadata = (await contract.get(token)).metadata;
+    const owner = await meltyfi.call("getLotteryOwner", lottery);
+    return {contract, token, name: metadata.name, image: metadata.image, owner};
+}
+
+async function getLotteryInfo(meltyfi, lottery){
+    let [expiration, wonkaBarPrice, wonkaBarsMaxSupply, wonkaBarsSold] = await Promise.all([
+        meltyfi.call("getLotteryExpirationDate", lottery),
+        meltyfi.call("getLotteryWonkaBarPrice", lottery),
+        meltyfi.call("getLotteryWonkaBarsMaxSupply", lottery),
+        meltyfi.call("getLotteryWonkaBarsSold", lottery),
+    ]);
+    
+    const remainingWonkaBars = await (wonkaBarsMaxSupply - wonkaBarsSold);
+      
+    return {expiration, wonkaBarPrice, wonkaBarsMaxSupply, remainingWonkaBars};
+}
+
+async function getLotteries(sdk){
+    const meltyfi = await sdk.getContract(addressMeltyFiNFT, MeltyFiNFT);
+    const lotteriesId = await meltyfi.call("activeLotteryIds");
+    const lotteries = new Array();
+
+    for (const lottery of lotteriesId){
+        const {expiration, wonkaBarPrice, wonkaBarsMaxSupply, remainingWonkaBars} = await getLotteryInfo(meltyfi, lottery);
+        const {contract, token, name, image} = await getNFTData(sdk, meltyfi, lottery);
+        lotteries.push({expiration, wonkaBarPrice, wonkaBarsMaxSupply, remainingWonkaBars, contract, token, name, image})
+    }
+    return lotteries;
+
+}
+
+
+function useRenderLotteries(sdk) {
+    const [lotteries, setLotteries] = useState([]);
+    useEffect(() => {
+        getLotteries(sdk).then(setLotteries)
+    });
     const buyWonkaBars = () => {alert("ciao")}
+    
     return lotteries.map((lottery) => {
+        const date = new Date(parseInt(lottery.expiration) * 1000);
+        const dateString = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        const text = <p>
+            <li> Expiry date: {dateString} </li>
+            <li> WonkaBar price: {parseInt(lottery.wonkaBarPrice)}</li>
+            <li> Remaining WonkaBars: {parseInt(lottery.remainingWonkaBars)}</li>
+            </p>;
         return <Col>
             {LotteryCard({
                 src: lottery.image,
@@ -22,54 +71,14 @@ function renderLotteries(lotteries) {
     });
 }
 
-async function loadMetadata(sdk, meltyfi, lottery) {
-    lottery = parseInt(lottery);
-    const contract = await sdk.getContract(await meltyfi.call("getLotteryPrizeContract", lottery), "nft-collection");
-    const token = await meltyfi.call("getLotteryPrizeTokenId", lottery);
-    const metadata = (await contract.get(token)).metadata;
-    const owner = await meltyfi.call("getLotteryOwner", lottery);
-    return {lottery, name: metadata.name, image: metadata.image, owner};
-}
 
-async function getLotteryInfo(sdk, addressMeltyFiNFT){
-    const meltyfi = await sdk.getContract(addressMeltyFiNFT, MeltyFiNFT);
-
-    const lotteries = await meltyfi.call("activeLotteryIds");
-
-    const lottery = lotteries[0];
-
-    const expiration = await meltyfi.call("getLotteryExpirationDate", lottery);
-
-    const wonkaBarPrice = await meltyfi.call("getLotteryWonkaBarPrice", lottery);
-
-    const wonkaBarMaxSupply = await meltyfi.call("getLotteryWonkaBarMaxSupply", lottery);
-    const wonkaBarSold = await meltyfi.call("getLotteryWonkaBarsSold", lottery);
-    const remainingWonkaBar = wonkaBarMaxSupply - wonkaBarSold;
-
-    const metaData = loadMetadata(sdk, meltyfi, lottery);
-
-}
-
-
-function Lotteries(props) {
+function Lotteries() {
     const sdk = new ThirdwebSDK("goerli");
-    getLotteryInfo(sdk, props.addressMeltyFiNFT);
 
     return (
         <Container>
             <Row>
-                <Col>
-                    <LotteryCard src="https://i0.wp.com/technoblitz.it/wp-content/uploads/2018/06/immagini-4k.jpg?w=925&ssl=1" />
-                </Col>
-                <Col>
-                    <LotteryCard src="https://media.elrond.com/nfts/asset/Qmf9DdY4tzgU3JEBe8TE88X2uD5pJfAYZRdLvXruRiF1uw/8146.png" />
-                </Col>
-                <Col>
-                    <LotteryCard src="https://media.elrond.com/nfts/asset/Qmf9DdY4tzgU3JEBe8TE88X2uD5pJfAYZRdLvXruRiF1uw/8146.png" />
-                </Col>
-                <Col>
-                    <LotteryCard src="https://media.elrond.com/nfts/asset/Qmf9DdY4tzgU3JEBe8TE88X2uD5pJfAYZRdLvXruRiF1uw/8146.png" />
-                </Col>
+                {useRenderLotteries(sdk)}
             </Row>
         </Container>
     );
