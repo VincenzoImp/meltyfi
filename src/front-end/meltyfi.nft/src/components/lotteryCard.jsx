15f3fbc69317f9cb8dd0ba9993131506e136f831
Princic-1837592 <andreapri.99@gmail.com>
2023-01-10 10:28:42 +0100

    lotteryCard

--- src/front-end/meltyfi.nft/src/components/lotteryCard.jsx
+++ src/front-end/meltyfi.nft/src/components/lotteryCard.jsx
@@ -1,20 +1,24 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function LotteryCard({ src }) {
+function LotteryCard({src, name, text, onClickFunction, onClickText}) {
+    let button = undefined;
+    if (onClickFunction !== undefined) {
+        button = <Button className='CardButton' onClick={onClickFunction}>{onClickText}</Button>;
+    }
     return (
         <Card className='Card'>
-            <Card.Img className='CardImg' src={src} />
+            <Card.Img className='CardImg' src={src}/>
             <Card.Body className='CardBody'>
                 <Card.Title className='CardTitle'>
-                    Card Title
+                    {name}
                 </Card.Title>
                 <Card.Text className='CardText'>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    {text}
                 </Card.Text>
-                <Button className='CardButton'>Go somewhere</Button>
+                {button}
             </Card.Body>
-        </Card >
+        </Card>
     );
 }
 
