6676ef5c8193a44e5bdc184a01ff1d424af9cd48
VincenzoImp <61617279+VincenzoImp@users.noreply.github.com>
2023-01-11 17:37:37 +0100

    report updates

--- src/back-end/MeltyFiProtocol/contracts/ChocoChip.sol
+++ src/back-end/MeltyFiProtocol/contracts/ChocoChip.sol
@@ -7,17 +7,8 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 
-contract ChocoChip is
-    ERC20,
-    ERC20Burnable,
-    ERC20Snapshot,
-    Ownable,
-    ERC20Permit,
-    ERC20Votes,
-    ERC20FlashMint
-{
+contract ChocoChip is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, ERC20Permit, ERC20Votes {
     constructor() ERC20("Choco Chip", "CHOC") ERC20Permit("Choco Chip") {}
 
     function snapshot() public onlyOwner {
@@ -30,19 +21,17 @@ contract ChocoChip is
 
     // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20, ERC20Snapshot) {
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Snapshot)
+    {
         super._beforeTokenTransfer(from, to, amount);
     }
 
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20, ERC20Votes) {
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
         super._afterTokenTransfer(from, to, amount);
     }
 
