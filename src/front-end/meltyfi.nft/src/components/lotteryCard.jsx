784505fa906153c693b8d1efad0362b7934f9307
Princic-1837592 <andreapri.99@gmail.com>
2023-01-11 11:58:18 +0100

    added `collection` param to card constructor

--- src/front-end/meltyfi.nft/src/components/lotteryCard.jsx
+++ src/front-end/meltyfi.nft/src/components/lotteryCard.jsx
@@ -1,7 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function LotteryCard({src, name, text, lotteryId, action}) {
+function LotteryCard({src, name, collection, text, lotteryId, action}) {
     let button = undefined;
     /*if (onClickFunction !== undefined) {
         button = <Button className='CardButton' onClick={onClickFunction}>{onClickText}</Button>;
@@ -11,14 +11,13 @@ function LotteryCard({src, name, text, lotteryId, action}) {
             <Card.Img className='CardImg' src={src}/>
             <Card.Body className='CardBody'>
                 <Card.Title className='CardTitle'>
-                    {name}
+                    {name} @ {collection}
                 </Card.Title>
                 <Card.Text>
                     Lottery #{lotteryId}
                 </Card.Text>
                 {text}
                 {action}
-
             </Card.Body>
         </Card>
     );
