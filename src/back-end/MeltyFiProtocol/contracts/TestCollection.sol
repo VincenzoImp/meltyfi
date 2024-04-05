956d01649a9ebdcbb12070025043ed3d1bf7dfed
VincenzoImp <vincenzoimperati99@gmail.com>
2024-04-05 20:27:15 +0200

    Update TestCollection.sol

--- src/back-end/MeltyFiProtocol/contracts/TestCollection.sol
+++ src/back-end/MeltyFiProtocol/contracts/TestCollection.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestCollection is ERC721 {
-    using Counters for Counters.Counter;
+contract TestCollection is ERC721, Ownable {
+    uint256 private _nextTokenId;
 
-    Counters.Counter private _tokenIdCounter;
+    constructor(address initialOwner)
+        ERC721("MeltyFi Test", "MFT")
+        Ownable(initialOwner)
+    {}
 
-    constructor() ERC721("MeltyFi Test", "MFT") {}
-
-    function safeMint(address to) public {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
     }
 }
